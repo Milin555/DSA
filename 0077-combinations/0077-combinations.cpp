159397b9fdeee59de5a4069997778b1c7389e621
milin dholiya <dholiyamilin@gmail.com>
2026-03-03 18:34:54 +0530

    Time: 258 ms (7.44%), Space: 140 MB (8.16%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void find(int st,vector<vector<int>> &res, vector<int> ans ,int n, int k){
+
+        if(ans.size()==k){
+            res.push_back(ans);
+            return ;
+
+        }
+
+        for(int i=st;i<=n;i++){
+            ans.push_back(i);
+            find(i+1,res,ans,n,k);
+            ans.pop_back();
+        }
+    }
+
+
+    vector<vector<int>> combine(int n, int k) {
+
+        vector<vector<int>> res;
+
+        vector<int> ans;
+
+        find(1,res,ans,n,k);
+        
+        return res;
+    }
+};
